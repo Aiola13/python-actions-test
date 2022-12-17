8bb4440f040aba1d74dc43408bbeeba020763631
Aiola <7544268+Aiola13@users.noreply.github.com>
2022-12-17 09:55:36 +0100

    Create send_email.py

---
+++ send_email.py
@@ -0,0 +1,20 @@
+import smtplib, ssl, os
+from email.message import EmailMessage
+
+SMTP_SERVER = os.environ.get('SMTP_SERVER')
+EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_RECIPIENT = os.environ.get('EMAIL_RECIPIENT')
+PORT = 465  # For SSL
+
+
+msg = EmailMessage()
+msg.set_content("Hello Underworld!")
+msg['Subject'] = "Hello Underworld from Python Scripts!"
+msg['From'] = EMAIL_ADDRESS
+msg['To'] = EMAIL_RECIPIENT
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(SMTP_SERVER, PORT, context=context) as server:
+    server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    server.send_message(msg, from_addr=EMAIL_ADDRESS, to_addrs=EMAIL_RECIPIENT)
